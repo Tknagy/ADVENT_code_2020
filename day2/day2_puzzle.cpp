62ea1940eaca638d70fdfc217f1e813a8e11cc67
Curti-Windows\Curti <joaocurti@hotmail.com>
2020-12-02 23:05:20 +0100

    added puzzle 1 of day 2

--- day2/day2_puzzle.cpp
+++ day2/day2_puzzle.cpp
@@ -11,30 +11,53 @@ bool isPasswordValid(std::string inputString)
     char relevantChar = '0';
     std::string password = "";
     std::string tempString = "";
-    tempString = inputString.substr(0, inputString.find("-")); // get first number (atLeast)
-    std::cout << "Tempstring for atLeast: " << tempString << std::endl;
-    atLeast = std::stoi (tempString);
-    tempString = inputString.substr(1, inputString.find("-")); // get second number (atMost)
-     std::cout << "Tempstring for atMost: " << tempString << std::endl;
-    atMost = std::stoi (tempString);
-    tempString = inputString.substr(1, inputString.find(" ")); // get charactere (relevantChar)
-    relevantChar = tempString[0];
-    password = inputString.substr(2, inputString.find(" ")); // get password 
+    std::string rangeString = "";
+    std::size_t tempIndex = 0;
 
-    std::size_t positionRelevantChar = password.find_first_of(relevantChar);
-    while (positionRelevantChar != std::string::npos)
+    //First, get the first segment of line with atLeast-atMost:
+    tempIndex = inputString.find(" ");
+    rangeString = inputString.substr(0, tempIndex); // get first and second numbers (atLeast and atMost)
+    inputString = inputString.substr(tempIndex+1);
+    //std::cout << "Tempstring for atLeast and atMost: " << rangeString << std::endl;
+    
+    //Then, get the second segment of line with the relevant Char:
+    tempIndex = inputString.find(" ");
+    tempString = inputString.substr(0, tempIndex); // get charactere (relevantChar)
+    relevantChar = tempString[0];
+    
+    //std::cout << "Tempstring for relevantChar: " << tempString << std::endl;
+    //Then, get the password substring
+    password = inputString.substr(tempIndex+1); // get password 
+    std::cout << "Tempstring for password: " << password << std::endl;
+
+    //Now, convert the rangeString to two integers and save it to atLeast and atMost
+    tempString = rangeString.substr(0, rangeString.find("-")); // get atLeast inside rangeString
+    //std::cout << "Tempstring for atLeast: " << tempString << std::endl;
+    atLeast = std::stoi (tempString);
+    tempIndex = rangeString.find("-")+1;
+
+    tempString = rangeString.substr(tempIndex); // get atLeast
+    //std::cout << "Tempstring for atLeast: " << tempString << std::endl;
+    atMost = std::stoi (tempString);
+
+    
+    //Now, get the amount of relevant char in the password string
+    tempIndex = password.find(relevantChar);
+    while (tempIndex != std::string::npos) // When it is not found anymore
     {
         occurences++;
-        positionRelevantChar = password.find_first_of(positionRelevantChar+1);
-        std::cout << occurences << " occurences of " << relevantChar << "found on  '" << password << "'" << std::endl; 
+        tempIndex = password.find(relevantChar,tempIndex+1);
+        
     }
-
+    //std::cout << occurences << " occurences of " << relevantChar << " found on  '" << password << "'. Range: " << rangeString ; 
     if(occurences >= atLeast && occurences <= atMost )
-    {
+    {   
+       // std::cout << ", password is VALID." << std::endl ; 
         return true;
     }
     else
-    {
+    {   
+     //   std::cout << " , password is INVALID." << std::endl ; 
         return false;  
     }
 
