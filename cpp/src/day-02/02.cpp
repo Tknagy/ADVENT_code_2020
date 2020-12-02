a7702f69f286be979102f6db43be155fabf9e448
Guilherme Lima <guilima10@gmail.com>
2020-12-02 21:42:33 +0100

    day-02

---
+++ cpp/src/day-02/02.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <cstdint>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <tuple>
+
+static std::vector<std::string> read_stdin()
+{
+    std::vector<std::string> input;
+    std::string line;
+    while (std::getline(std::cin, line))
+        input.push_back(line);
+    return input;
+}
+
+std::tuple<std::uint32_t, std::uint32_t, char, std::string> parse_information(const std::string &line)
+{
+    int first, second;
+    char letter;
+    sscanf(line.c_str(), "%d-%d %c:", &first, &second, &letter);
+    std::string password(line.substr(line.find(":")+2));
+
+    return std::make_tuple(first, second, letter, password);
+}
+
+static bool part_one(const std::string &line)
+{
+    auto [min, max, letter, password] = parse_information(line);
+    auto qty = std::count(password.begin(), password.end(), letter);
+    return (min <= qty && qty <= max) ? true : false;
+}
+
+static bool part_two(const std::string &line)
+{
+    auto [first, second, letter, pwd] = parse_information(line);
+    return ((pwd[first-1] == letter) != (pwd[second-1] == letter)) ? true : false;
+}
+
+int main(int argc, char const *argv[])
+{
+    std::vector<std::string> input = read_stdin();
+    std::vector<bool> first, second;
+    std::transform(input.begin(), input.end(), std::back_inserter(first), part_one);
+    std::transform(input.begin(), input.end(), std::back_inserter(second), part_two);
+    printf("%ld\n", std::count(first.begin(), first.end(), true));
+    printf("%ld\n", std::count(second.begin(), second.end(), true));
+    return 0;
+}
