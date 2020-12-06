8d1d09fdecadf2b89fe9fbe077b1a1764da084ed
João Pedro Curti <joaocurti@hotmail.com>
2020-12-06 17:47:02 +0100

    refractored and added puzzle 2

--- day4/day4_puzzle_v2.cpp
+++ day4/day4_puzzle_v2.cpp
@@ -18,37 +18,35 @@
 // cid ->(Country ID) -> NOT RELEVANT FOR PUZZLE 1
 //If the above fields are there, then consider it as a valid passport
 
-uint8_t validatePassport(std::string passportLine)
-{
-    if(passportLine.find("byr:") != std::string::npos &&
-    passportLine.find("iyr:") != std::string::npos &&
-    passportLine.find("eyr:") != std::string::npos &&
-    passportLine.find("hgt:") != std::string::npos &&
-    passportLine.find("hcl:") != std::string::npos &&
-    passportLine.find("ecl:") != std::string::npos &&
-    passportLine.find("pid:") != std::string::npos )return 1;
-    else return 0;
 
+bool getValueWithKey(std::string inputString, std::string regexString,bool returnString=false, std::string *returnedValue = NULL)
+{
+    auto const internRegex = std::regex(regexString);
+    auto const spaceRegex = std::regex(" ");
+    auto positionOfValueField = std::smatch{};
+    bool const myTextContainsRegex = std::regex_search(inputString, positionOfValueField, internRegex);
+    if(returnString && myTextContainsRegex)
+    {   
+        inputString = inputString.substr(positionOfValueField.prefix().length()+regexString.length());//gets substring starting from key
+        std::regex_search(inputString, positionOfValueField, spaceRegex);// executes regex now to find position of " " a.k.a end of value field
+        inputString = inputString.substr(0,positionOfValueField.prefix().length());//gets value substring until a space is found
+        
+        returnedValue->append(inputString);
+    }
+    
+    return myTextContainsRegex;
 }
 
-uint8_t validatePassportWithRegex(std::string passportLine)
-{   
-    auto const regexByr = std::regex("byr:");
-    auto const regexIyr = std::regex("iyr:");
-    auto const regexEyr = std::regex("eyr:");
-    auto const regexHgt = std::regex("hgt:");
-    auto const regexHcl = std::regex("hcl:");
-    auto const regexEcl = std::regex("ecl:");
-    auto const regexPid = std::regex("pid:");
-    return std::regex_search(passportLine, regexByr) && 
-    std::regex_search(passportLine, regexIyr) && 
-    std::regex_search(passportLine, regexEyr) && 
-    std::regex_search(passportLine, regexHgt) && 
-    std::regex_search(passportLine, regexHcl) && 
-    std::regex_search(passportLine, regexEcl) && 
-    std::regex_search(passportLine, regexPid);
+uint8_t validatePassport_Puzzle1(std::string passportLine)
+{      
+    return getValueWithKey(passportLine,"byr:",false,NULL) &&
+    getValueWithKey(passportLine,"iyr:",false,NULL) &&
+    getValueWithKey(passportLine,"eyr:",false,NULL) &&
+    getValueWithKey(passportLine,"hgt:",false,NULL) &&
+    getValueWithKey(passportLine,"hcl:",false,NULL) &&
+    getValueWithKey(passportLine,"ecl:",false,NULL) &&
+    getValueWithKey(passportLine,"pid:",false,NULL);
     
-
 }
 //Puzzle 2 Objective: validate the data as follows
 // byr (Birth Year) - four digits; at least 1920 and at most 2002.
@@ -62,74 +60,113 @@ uint8_t validatePassportWithRegex(std::string passportLine)
 // pid (Passport ID) - a nine-digit number, including leading zeroes.
 // cid (Country ID) - ignored, missing or not.
 
-// uint8_t validatePassport_puzzle2(std::string passportLine)
-// {   
-//     int byr;
-//     int iyr;
-//     int eyr;
-//     std::string hgtString;
-//     int hgtValue;
-//     int hcl;
-//     std::string ecl;
-//     double pid;
-//     //int cid;
+
+
+uint8_t validatePassport_Puzzle2(std::string passportLine)
+{   
+    std::string byr;
+    std::string iyr;
+    std::string eyr;
+    std::string hgt;
+    std::string hcl;
+    std::string ecl;
+    std::string pid;
+    getValueWithKey(passportLine,"byr:",true,&byr);
+    getValueWithKey(passportLine,"iyr:",true,&iyr);
+    getValueWithKey(passportLine,"eyr:",true,&eyr);
+    getValueWithKey(passportLine,"hgt:",true,&hgt);
+    getValueWithKey(passportLine,"hcl:#",true,&hcl);
+    getValueWithKey(passportLine,"ecl:",true,&ecl);
+    getValueWithKey(passportLine,"pid:",true,&pid);
     
-//     if(sscanf(passportLine.substr(passportLine.find("byr:")+4).c_str(), "%d", &(byr)) )
-//     {
-//         if(byr <2002 || byr >1920) return 0;
-//     } 
-//     else return 0;
-//     if(sscanf(passportLine.substr(passportLine.find("iyr:")+4).c_str(), "%d", &(iyr)) )
-//     {
-//         if(iyr <2010 || iyr >2020) return 0;
-//     } 
-//     else return 0;
-//     if(sscanf(passportLine.substr(passportLine.find("eyr:")+4).c_str(), "%d", &(eyr)) )
-//     {
-//         if(eyr <2020 || eyr >2030) return 0;
-//     } 
-//     else return 0;
+
+    // byr (Birth Year) - four digits; at least 1920 and at most 2002.  
+    int byrDec;
+     try {
+        byrDec = std::stoi(byr);
+    }
+    catch (const std::invalid_argument& ia) {
+        std::cerr << "Invalid argument for the passport id: " << ia.what() << '\n';
+        return 0;
+    }
+    if(byrDec < 1920 || byrDec > 2002) return 0;
+
+    // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    int iyrDec;
+    try {
+        iyrDec = std::stoi(iyr);
+    }
+    catch (const std::invalid_argument& ia) {
+        std::cerr << "Invalid argument for the passport id: " << ia.what() << '\n';
+        return 0;
+    }
+    if(iyrDec < 2010 || iyrDec > 2020) return 0;
+
+    // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    int eyrDec;
+    try {
+        eyrDec = std::stoi(eyr);
+        std::cout << "eyrDec:" << eyrDec << std::endl;
+    }
+    catch (const std::invalid_argument& ia) {
+        std::cerr << "Invalid argument for the passport id: " << ia.what() << '\n';
+        return 0;
+    }
     
-//     std::size_t sizeOfHclField = passportLine.find(" ",passportLine.find("hcl:#")+5);
-//     if(sizeOfHclField == 6 && sscanf(passportLine.substr(passportLine.find("hcl:#")+5,sizeOfHclField).c_str(), "%x", &(hcl)) )
-//     {} 
-//     else return 0;
-//     std::size_t sizeOfPidField = passportLine.find(" ",passportLine.find("pid:")+4);
-//     if(sizeOfPidField==9 && sscanf(passportLine.substr(passportLine.find("pid:")+4,sizeOfPidField ).c_str(), "%d", &(pid)))
-//     {}
-//     else return 0;
+    if(eyrDec < 2020 || eyrDec > 2030) return 0;
+
+    // hgt (Height) - a number followed by either cm or in:
+    // If cm, the number must be at least 150 and at most 193.
+    // If in, the number must be at least 59 and at most 76.
+    int hgtDec;
+    if(getValueWithKey(hgt,"cm",false,NULL))
+    {
+        hgtDec = std::stoi(hgt.substr(0, hgt.size()-2));
+        if(hgtDec < 150 || hgtDec > 193) return 0;
+    }else if(getValueWithKey(hgt,"in",false,NULL))
+    {
+        hgtDec = std::stoi(hgt.substr(0, hgt.size()-2));
+        if(hgtDec < 59 || hgtDec > 76) return 0;
+    }else return 0; // if cm nor in is present on hgt String
+
+    // hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    if(hcl.size() == 6)
+    {   
+        //try to read the value that is expected to be an HEX
+        try {
+           int hclHex = std::stoi(hcl,nullptr,16);
+        }
+        catch (const std::invalid_argument& ia) {
+            std::cerr << "Invalid argument for the hair color: " << ia.what() << '\n';
+            return 0;
+        }
+        
+    }else return 0; // if the length is not 6, it isn't valid
     
-//     ecl = passportLine.substr(passportLine.find("ecl:")+4);
-//     if(ecl == "amb" || ecl == "blu" || ecl == "brn" || ecl == "gry" || ecl == "grn" || ecl == "hzl" || ecl == "oth" )
-//     {
-//     } 
-    
-//     //For the hgt field, check if it is in cm or in
-//     passportLine.find("cm",passportLine.find("hgt:")+4);
-//     std::size_t sizeOfHgtField = passportLine.find("cm",passportLine.find("hgt:")+4);
-//     if( sizeOfHgtField != std::string::npos)
-//     {   
-//         //verify for cm
-//         sscanf(passportLine.substr(passportLine.find("hgt:")+4,sizeOfHgtField).c_str(),"%d",&hgtValue );
-//         // If cm, the number must be at least 150 and at most 193.
-//         if(hgtValue <150 || hgtValue >193 )return 0;          
-//     }
-//     else return 0;
+    // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    if(ecl == "amb" || ecl == "blu" || ecl == "brn" || ecl == "gry" || ecl == "grn" || ecl == "hzl" || ecl == "oth" )
+    {
 
+    }
+    else return 0;
+    // pid (Passport ID) - a nine-digit number, including leading zeroes.
+    if(pid.length()==9)
+    {
+        //try to read the value that is expected to be a number
+        try {
+           long long pidDec = std::stoll(pid);
+        }
+        catch (const std::invalid_argument& ia) {
+            std::cerr << "Invalid argument for the passport id: " << ia.what() << '\n';
+            return 0;
+        }
+    }
+    else return 0;
+    // cid (Country ID) - ignored, missing or not. So, doesnt even consider it
+    //If all criterias passed, then return true a.k.a the passport is valid
+    return 1;
 
-
-//     // If in, the number must be at least 59 and at most 76.
-//     else 
-//     {
-//         std::cerr << "Error in the hgt field of one Passport - type undefined" << std::endl;
-//         tempPassport->hgtFormat = "not specified";
-//         sscanf(hgtTempString.c_str(), "%d", &(tempPassport->hgtValue));
-
-//     }   
-
-//     return 1;
-
-// }
+}
 
 int main(int argc, char *argv[])
 {   
@@ -149,12 +186,12 @@ int main(int argc, char *argv[])
     passports.push_back("");
     while ( std::getline(entries, inputLine) )
     {   
-        if(!inputLine.empty()) passports.back().append(inputLine);
+        if(!inputLine.empty()) passports.back().append(inputLine+ " ");
         else passports.push_back("");
     }
     entries.close();
     std::vector<uint8_t> passportCounter;
-    std::transform(passports.begin(),passports.end(), std::back_inserter(passportCounter),validatePassportWithRegex);
+    std::transform(passports.begin(),passports.end(), std::back_inserter(passportCounter),validatePassport_Puzzle2);
 
     std::cout << "result puzzle 1: " << std::accumulate(passportCounter.begin(),passportCounter.end(),0) << std::endl;
 
