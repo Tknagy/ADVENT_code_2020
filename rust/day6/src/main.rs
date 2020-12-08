ec6adc09928e69a913332d3ef59c14337593b516
pidgey <lpdon2@gmail.com>
2020-12-09 00:18:38 +0100

    make it more rust style

--- rust/day6/src/main.rs
+++ rust/day6/src/main.rs
@@ -2,43 +2,39 @@ use std::collections::HashSet;
 use std::fs;
 
 fn part1(group_answers: &Vec<&str>) {
-    let mut all_answers = 0;
-
-    for answer in group_answers {
-        let answer: String = answer.split_whitespace().collect();
-        let group_unique_answers: HashSet<char> = answer.chars().collect();
-        all_answers += group_unique_answers.len();
-    }
+    let all_answers: usize = group_answers
+        .iter()
+        .map(|answer| {
+            let answer: String = answer.split_whitespace().collect();
+            let n_unique_answers = answer.chars().collect::<HashSet<char>>().len();
+            n_unique_answers
+        })
+        .sum();
 
     println!("{}", all_answers);
 }
 
 fn part2(group_answers: &Vec<&str>) {
-    let mut all_answers = 0;
-
-    for group_answer in group_answers {
-        let mut group_unique_answers: HashSet<char> = HashSet::new();
-        let person_answers: Vec<&str> = group_answer.split_whitespace().collect();
-
-        for person_answer in &person_answers {
-            let person_answer = person_answer.to_string();
-            let person_unique_answers: HashSet<char> = person_answer.chars().collect();
-            group_unique_answers = group_unique_answers
-                .union(&person_unique_answers)
-                .cloned()
+    let all_answers: usize = group_answers
+        .iter()
+        .map(|group_answer| {
+            let mut group_unique_answers: HashSet<char> = group_answer
+                .chars()
+                .filter(|c| !c.is_whitespace())
                 .collect();
-        }
 
-        for person_answer in &person_answers {
-            let person_unique_answers: HashSet<char> = person_answer.chars().collect();
-            group_unique_answers = group_unique_answers
-                .intersection(&person_unique_answers)
-                .cloned()
-                .collect();
-        }
+            let person_answers: Vec<&str> = group_answer.split_whitespace().collect();
+            for person_answer in &person_answers {
+                let person_unique_answers: HashSet<char> = person_answer.chars().collect();
+                group_unique_answers = group_unique_answers
+                    .intersection(&person_unique_answers)
+                    .cloned()
+                    .collect();
+            }
 
-        all_answers += group_unique_answers.len();
-    }
+            group_unique_answers.len()
+        })
+        .sum();
 
     println!("{}", all_answers);
 }
